41267698232a94ea8c9a06430b0adbc93e9964fa
Nguyen Quang Duy <ngdyyy06@gmail.com>
2026-08-19 17:23:16 +0700

    Remove dotenv usage for API key configuration
    Removed dotenv configuration and related code.

--- src/main/java/weatherapp/WeatherappApplication.java
+++ src/main/java/weatherapp/WeatherappApplication.java
@@ -1,6 +1,5 @@
 package weatherapp;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeatherappApplication {
 
 	public static void main(String[] args) {
-//		Dotenv dotenv = Dotenv.configure()
-//						.ignoreIfMissing()
-//								.load();
-//
-//		System.setProperty("WEATHER_API_KEY", dotenv.get("WEATHER_API_KEY"));
 
 		SpringApplication.run(WeatherappApplication.class, args);
 	}
